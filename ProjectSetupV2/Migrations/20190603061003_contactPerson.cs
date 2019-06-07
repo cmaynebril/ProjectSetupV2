1d6c919a3465bde32ca8106d5cff84ec5fc2e15a
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-07 10:36:08 +0800

    migrations

---
+++ ProjectSetupV2/Migrations/20190603061003_contactPerson.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectSetupV2.Migrations
+{
+    public partial class contactPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPerson",
+                table: "Clients",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactPerson",
+                table: "Clients");
+        }
+    }
+}
