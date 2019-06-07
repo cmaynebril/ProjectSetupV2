1b0da499dd7f8cbf36b4a80f79f3c4d1f7aba909
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-07 10:36:27 +0800

    viewmodel only

---
+++ ProjectSetupV2/Models/Context/InvoiceViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class InvoiceViewModel
+    {
+        public string Client { get; set; }
+        public string ContactPerson { get; set; }
+        public string Job { get; set; }
+        public double? JobRate { get; set; }
+        public string Task { get; set; }
+        public double? TasksRate { get; set; }
+        public string Business { get; set; }
+        public double Rate { get; set; }
+        public string Assignee { get; set; }
+        public long? AssigneeRate { get; set; }
+
+    }
+}
