9af6a3995fdebf0694779c266aa88de5039941b3
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-14 08:52:51 +0800

    Change TimeSpent to TotalTime

---
+++ ProjectSetupV2/Migrations/20190614004849_JobTasks009.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectSetupV2.Migrations
+{
+    public partial class JobTasks009 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeSpent",
+                table: "JobTasks",
+                newName: "TotalTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalTime",
+                table: "JobTasks",
+                newName: "TimeSpent");
+        }
+    }
+}
