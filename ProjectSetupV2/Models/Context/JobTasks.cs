94f0bbd948c1d28221889374fa4067e63be04c9f
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-14 08:53:53 +0800

    JobTasks Model

---
+++ ProjectSetupV2/Models/Context/JobTasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class JobTasks
+    {
+        public long Id { get; set; }
+        public DateTime? Date { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public double TotalTime { get; set; }
+
+        public Tasks Task { get; set; } //this should be foreign key to table task
+        public Jobs Job { get; set; } //this should be foreign key to table job
+        public Clients Client { get; set; } //this should be foreign key to table client
+        public BusinessValues BusinessValue { get; set; } //this should be foreign key to table businessvalue
+        public Assignees Assignee { get; set; } //this should be foreign key to table assignee
+    }
+}
