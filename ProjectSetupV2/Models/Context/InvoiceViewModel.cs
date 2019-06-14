c747d6bbd146e6cab914d72a0138c34a121a517e
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-14 08:55:10 +0800

    InvoiceViewModel

--- ProjectSetupV2/Models/Context/InvoiceViewModel.cs
+++ ProjectSetupV2/Models/Context/InvoiceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,58 @@ namespace ProjectSetupV2.Models.Context
 {
     public class InvoiceViewModel
     {
-        public string Client { get; set; }
-        public string ContactPerson { get; set; }
+        public InvoiceViewModel()
+        {
+            customers = new List<iCustomers>();
+            jobs = new List<iJobs>();
+            tasks = new List<iTasks>();
+            businessValues = new List<iBusinessValues>();
+        }
+
+        public long Id { get; set; }
+        public string CustomerName { get; set; }
+        public long? CustomerId { get; set; }
         public string Job { get; set; }
+        public string TaskName { get; set; }
+        public string BusinessValue { get; set; }
+
+        public IList<iCustomers> customers { get; set; }
+        public IList<iJobs> jobs { get; set; }
+        public IList<iTasks> tasks { get; set; }
+        public IList<iBusinessValues> businessValues { get; set; }
+    }
+
+    public class iCustomers
+    {
+        public long Id { get; set; }
+        public string CustomerName { get; set; }
+    }
+
+    public class iJobs
+    {
+        public long Id { get; set; }
+        public string Job { get; set; }
+        public string CustomerName { get; set; }
+        public long? CustomerId { get; set; }
         public double? JobRate { get; set; }
-        public string Task { get; set; }
-        public double? TasksRate { get; set; }
-        public string Business { get; set; }
-        public double Rate { get; set; }
-        public string Assignee { get; set; }
-        public long? AssigneeRate { get; set; }
 
     }
+
+    public class iTasks
+    {
+        public long Id { get; set; }
+        public string Task { get; set; }
+        public long? JobId { get; set; }
+        public long? BusinessValuesId { get; set; }
+        public double? TasksRate { get; set; }
+        public string Status { get; set; }
+    }
+
+        public class iBusinessValues
+    {
+        public long Id { get; set; }
+        public string Business { get; set; }
+        public double Rate { get; set; }
+    }
+
 }
