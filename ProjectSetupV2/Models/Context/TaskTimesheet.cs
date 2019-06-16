a5d916ca3a221abd4aba5f87e723706e481baf7a
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-17 07:38:27 +0800

    models

---
+++ ProjectSetupV2/Models/Context/TaskTimesheet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class TaskTimesheet
+    {
+        public int Id { get; set; }
+        public string Client { get; set; }
+        public string Job { get; set; }
+        public string Task { get; set; }
+        public DateTime? Date { get; set; }
+        public string TotalTimeSpent { get; set; }
+        public int AssigneeId { get; set; }
+    }
+}
---
+++ ProjectSetupV2/Models/UserTasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models
+{
+    public class UserTasks
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string TotalTime { get; set; }
+
+
+
+
+    }
+}
