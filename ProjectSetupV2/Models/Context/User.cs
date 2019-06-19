356abd73c6a2b0acc0c9cca9daeb986c56306a41
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-19 09:17:59 +0800

    user login

---
+++ ProjectSetupV2/Models/Context/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class User : IdentityUser <int>
+    {
+        public int TeamLeaderId { get; set; }
+        public int SupervisorId { get; set; }
+        public int ManagerId { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Section { get; set; }
+    }
+}
