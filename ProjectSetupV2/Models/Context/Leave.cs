70498dfe0b05a53ede860e864de91e6325d870a4
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-19 09:17:35 +0800

    leave & leave approver

---
+++ ProjectSetupV2/Models/Context/Leave.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class Leave
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime? StartDate { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime? EndDate { get; set; }
+        public string  Reason { get; set; }
+        public string Status { get; set; }
+        public string TypeOfRequest { get; set; }
+
+        public User User { get; set; }
+        public IEnumerable<LeaveApprover> LeaveApprover { get; set; }
+    }
+}
---
+++ ProjectSetupV2/Models/Context/LeaveApprover.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class LeaveApprover
+    {
+        [Key]
+        public int ApproverId { get; set; }
+        public string Approver { get; set; }
+        public int LeaveId { get; set; }
+        public string Status { get; set; }
+
+        public Leave Leave { get; set; }
+    }
+}
