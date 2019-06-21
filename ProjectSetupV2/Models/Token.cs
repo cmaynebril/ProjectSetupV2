51768b2afe51aa3035aaac33a0806b369edb086d
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-21 16:02:45 +0800

    Token model

---
+++ ProjectSetupV2/Models/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models
+{
+    public class Token
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
