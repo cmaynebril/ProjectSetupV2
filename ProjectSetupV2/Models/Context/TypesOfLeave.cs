51cad1bc901d1c2c113a4962231318dbaf0d95c3
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-28 09:24:55 +0800

    Types of Leave

---
+++ ProjectSetupV2/Models/Context/TypesOfLeave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSetupV2.Models.Context
+{
+    public class TypesOfLeave
+    {
+        public int Id { get; set; }
+        public string Leave { get; set; }
+    }
+}
